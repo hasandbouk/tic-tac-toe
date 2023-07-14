d7244dfc38cdd56882b0baa076fecb15db3403e8
Hasan <hasan@Hasans-MacBook-Air.local>
2023-07-14 11:18:48 +0300

    changed how the game interprets who's the current player

--- scripts.js
+++ scripts.js
@@ -2,19 +2,45 @@
 // const PLAYER_O_CLASS = document.getElementsByClassName("player-2-name");
 const cell_elements = document.querySelectorAll(".cell");
 const board_element = document.getElementById("board");
-let current_player = "x";
+let currentPlayer = "x";
 let score1 = 0;
 let score2 = 0;
 let p1Sequence = [];
 let p2Sequence = [];
-cell_elements.forEach((cell)=>{
-    cell.addEventListener('click', (e)=>{
-        console.log(e.target.innerText)
-    })
+
+
+cell_elements.forEach((cell) => {
+    cell.addEventListener('click', addTic);
 })
 
 
-const WINNING_COMBITNATIONS = [
+
+function addTic(e) {
+    const cell = e.target;
+    if (fullCells.includes(cell.id)) return;
+
+    if (currentPlayer === "x") {
+        cell.innerText = "x";
+        p1Sequence.push(Number(cell.id));
+        currentPlayer = "o";
+    } else {
+        cell.innerText = "o";
+        p2Sequence.push(Number(cell.id));
+        currentPlayer = "x";
+    }
+
+    // cell.innerText = currentPlayer;
+    fullCells.push(cell.id)
+    // currentPlayer = currentPlayer === "x"? "o":"x" 
+    // add check winner formula down
+}
+
+let fullCells = [];
+//check for a winner
+//game state
+//to restart the game with the score
+
+const winningSequence = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -24,3 +50,4 @@ const WINNING_COMBITNATIONS = [
     [0, 4, 8],
     [2, 4, 6],
 ];
+
